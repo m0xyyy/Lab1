41d172efb2a0fad5da8ce7b670aaa8da30928272
Your Name <your_email@example.com>
2023-10-02 20:07:08 +0300

    Added button and color change

--- lab1/app/src/main/java/com/example/lab1/MainActivity.kt
+++ lab1/app/src/main/java/com/example/lab1/MainActivity.kt
@@ -1,11 +1,38 @@
 package com.example.lab1
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btnChangeText = findViewById<Button>(R.id.btnChangeText)
+        val btnChangeColor = findViewById<Button>(R.id.btnChangeColor)
+
+        btnChangeText.setOnClickListener {
+            val textView = findViewById<TextView>(R.id.tvmain)
+            textView.text = "Button Clicked!"
+        }
+
+        btnChangeColor.setOnClickListener {
+            val textView = findViewById<TextView>(R.id.tvmain)
+            textView.setTextColor(Color.RED) // Change to the desired color
+        }
+    }
+
+    fun onBtnChangeTextClick(view: View) {
+        val textView = findViewById<TextView>(R.id.tvmain)
+        textView.text = "Button Clicked!"
+    }
+
+    fun onBtnChangeColorClick(view: View) {
+        val textView = findViewById<TextView>(R.id.tvmain)
+        textView.setTextColor(Color.RED) // Change to the desired color
     }
 }
